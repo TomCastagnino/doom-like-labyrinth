16b0879bc4fed428f3d523336e1ec4a06e66fb1a
DESK\Tom <tcastagnino@gmail.com>
2018-02-19 16:47:44 -0300

    Agrego archivos del juego

---
+++ Assets/Scripts/FPSInput.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class FPSInput : MonoBehaviour {
+
+    public float speed = 9;
+
+    private CharacterController charController;
+
+	// Use this for initialization
+	void Start () {
+        charController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveThePlayer();
+	}
+
+    void MoveThePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            MoveForward();
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            MoveBackward();
+        }
+    }
+
+    void MoveLeft()
+    {
+        float yAngle = transform.localEulerAngles.y;
+        yAngle += -1f;
+        transform.localEulerAngles = new Vector3(0, yAngle, 0);
+    }
+
+    void MoveRight()
+    {
+        float yAngle = transform.localEulerAngles.y;
+        yAngle += 1f;
+        transform.localEulerAngles = new Vector3(0, yAngle, 0);
+    }
+
+    void MoveForward()
+    {
+        Vector3 movement = new Vector3(0, 0, speed * Time.deltaTime);
+        movement = transform.TransformDirection(movement);
+        charController.Move(movement);
+    }
+
+    void MoveBackward()
+    {
+        Vector3 movement = new Vector3(0, 0, -(speed * Time.deltaTime));
+        movement = transform.TransformDirection(movement);
+        charController.Move(movement);
+    }
+
+}
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+    public float _rotationX = 0;
+    public float sensitivityVer = 9.0f;
+    public float sensitivityHor = 9.0f;
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVer;
+        _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+        float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+        float rotationY = transform.localEulerAngles.y + delta;
+        transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+	}
+}
